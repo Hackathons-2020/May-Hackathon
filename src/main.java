4c2e6eff148294c81b2c78cf4f0604988bace153
damani15 <rodamani10@gmail.com>
2020-05-26 18:36:27 -0400

    .

--- src/main.java
+++ src/main.java
@@ -6,8 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
     public class main {
-        public main() {
-        }
 
         public static void main(String[] args) {
             JFrame frame = new JFrame();
